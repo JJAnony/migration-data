681461033aa54894a031bb83cfb3e6db702a07be
anonymous <jcolivati@gmail.com>
2023-07-30 22:50:48 -0300

    big bang

---
+++ src/main/java/br/com/migrationdada/MigrationDadaApplication.java
@@ -0,0 +1,13 @@
+package br.com.migrationdada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationDadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationDadaApplication.class, args);
+	}
+
+}
